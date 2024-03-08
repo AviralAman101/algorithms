c3b7797d6374025dff741d7479143cb8946e4db2
Aviral Aman <78656202+AviralAman101@users.noreply.github.com>
2024-03-08 19:36:39 +0530

    Algorithm for LongestString-Zoho

--- in/algo/LongestRepeatingString.java
+++ in/algo/LongestRepeatingString.java
@@ -1,9 +1,6 @@
 package in.algo;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /*
 *
@@ -33,10 +30,58 @@ Explanation:
 The string "heh" is the longest Substring of s which is repeating but not overlapping.*/
 public class LongestRepeatingString {
     public static void main(String[] args) {
-        System.out.println(longestSubstring("abaabbbbbaa", 11));
+        System.out.println(longestSubstring("aaaa", 4));
     }
 
-    static String longestSubstring(String s, int n) {
+    static String longestSubstring(String s, int N) {
+        int[][] LCSRe = new int[N + 1][N + 1];
+
+        // Setting all to 0
+        for (int[] row : LCSRe) {
+            Arrays.fill(row, 0);
+        }
+
+        StringBuilder res = new StringBuilder(); // To store result
+        int res_length = 0; // To store length of result
+        int index = 0; // To store the starting index of the longest substring
+
+        // building table in bottom-up manner
+        for (int i = 1; i <= N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                // (j-i) > LCSRe[i-1][j-1] to remove
+                // overlapping
+                if (s.charAt(i - 1) == s.charAt(j - 1) &&
+                        LCSRe[i - 1][j - 1] < (j - i)) {
+                    LCSRe[i][j] = LCSRe[i - 1][j - 1] + 1;
+
+                    // updating maximum length of the
+                    // substring and updating the starting
+                    // index of the longest substring
+                    if (LCSRe[i][j] > res_length) {
+                        res_length = LCSRe[i][j];
+                        index = Math.max(i, index);
+                    }
+                } else {
+                    LCSRe[i][j] = 0;
+                }
+            }
+        }
+
+        // If we have non-empty result, then insert all
+        // characters from the starting index to the
+        // length of the longest substring
+        if (res_length > 0) {
+            for (int i = index - res_length + 1; i <= index; i++) {
+                res.append(s.charAt(i - 1));
+            }
+        } else {
+            res.append("-1");
+        }
+
+        return res.toString();
+    }
+
+    static String longestSubstringUnOptimized(String s, int n) {
         // code here
         String prefix="";
         String suffix="";
@@ -57,10 +102,12 @@ public class LongestRepeatingString {
 
             int j = b ? prefix.length()- sufLength :0;
             for (; j < prefix.length() && prefix.length()-j > lngLength; j++) {
-                //x++;
+
+//                x++;
 //                System.out.println(prefix.substring(j));
 //                System.out.println("prefix check ------------");
                 String ss = prefix.substring(j);
+                if(ss.length()<=lngLength) break;
                 if(suffix.contains(ss))
                     longest=ss;
             }
